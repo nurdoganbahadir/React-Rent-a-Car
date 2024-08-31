3277b1efa6fd8e0922642deccb356d9d28190236
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-09-01 01:08:54 +0300

    temalar ve butonu eklendi

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+import { useState } from "react";
 import "./App.css";
 import HomePage from "./Pages/HomePage";
+import { useEffect } from "react";
+import { MdModeNight } from "react-icons/md";
+import { FaSun } from "react-icons/fa";
 
 function App() {
+  const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
+
+  // Tema değişikliği olduğunda bu değişikliği kaydet ve body'ye uygula.
+  useEffect(() => {
+    document.body.className = theme;
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  // Temayı değiştiren fonksiyon
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  };
+
   return (
     <div className="App">
+      <button onClick={toggleTheme} className="dark-light">
+        {theme === "light" ? <MdModeNight /> : <FaSun />}
+      </button>
       <HomePage />
     </div>
   );
