745b0c984336ee7835de1bb6505fb0e40028ce9f
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-08-31 22:47:51 +0300

    data eklendi dosya yapısı oluşturuldu

---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "../components/Header/Header";
+
+import Footer from "../components/Footer/Footer";
+import Main from "../components/Main/Main.jsx";
+
+const HomePage = () => {
+  return (
+    <div>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+};
+
+export default HomePage;
